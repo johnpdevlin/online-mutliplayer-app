2df85d38ed1d1de4b59d7637fc1b30dad95c519f
John Devlin <johnp.devlin2@gmail.com>
2023-11-22 18:33:17 +0000

    make types readonly in fullDeck

--- src/data/deck.ts
+++ src/data/deck.ts
@@ -9,22 +9,24 @@ import {
 	WizardCard,
 } from '@/types/card';
 
-const jokerSet: JokerCard[] = [
+const jokerSet: readonly JokerCard[] = [
 	{ type: 'joker', cardID: 1 },
 	{ type: 'joker', cardID: 2 },
 	{ type: 'joker', cardID: 3 },
 	{ type: 'joker', cardID: 4 },
 ];
-const wizardSet: WizardCard[] = [
+const wizardSet: readonly WizardCard[] = [
 	{ type: 'wizard', cardID: 5 },
 	{ type: 'wizard', cardID: 6 },
 	{ type: 'wizard', cardID: 7 },
 	{ type: 'wizard', cardID: 8 },
 ];
 
-const cardColours: CardColour[] = ['red', 'green', 'blue', 'yellow'];
-const cardValues: CardValue[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-const colouredSet = (): ColouredCard[] => {
+const cardColours: readonly CardColour[] = ['red', 'green', 'blue', 'yellow'];
+const cardValues: readonly CardValue[] = [
+	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+];
+const colouredSet = (): readonly ColouredCard[] => {
 	const cards: ColouredCard[] = [];
 	let idNum: number = 9;
 	cardColours.forEach((colour) => {
@@ -42,6 +44,6 @@ const colouredSet = (): ColouredCard[] => {
 	return cards;
 };
 
-const fullDeck: Card[] = [...jokerSet, ...wizardSet, ...colouredSet()];
+const fullDeck: readonly Card[] = [...jokerSet, ...wizardSet, ...colouredSet()];
 
 export default fullDeck;
