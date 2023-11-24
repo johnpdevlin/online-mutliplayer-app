a3c1a88fdf8828aa9382f5c46fd94941cb38d602
John Devlin <johnp.devlin2@gmail.com>
2023-11-24 17:17:36 +0000

    install basic socket.io api route implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,33 @@
 /** @format */
 
 import Head from 'next/head';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import { io } from 'socket.io-client';
+import { useEffect } from 'react';
 
 export default function Home() {
+	useEffect(() => {
+		fetch('/api/socket').finally(() => {
+			const socket = io();
+
+			socket.on('connect', () => {
+				console.log('connect');
+				socket.emit('hello');
+			});
+
+			socket.on('hello', (data) => {
+				console.log('hello', data);
+			});
+
+			socket.on('a user connected', () => {
+				console.log('a user connected');
+			});
+
+			socket.on('disconnect', () => {
+				console.log('disconnect');
+			});
+		});
+	}, []); // Added [] as useEffect filter so it will be executed only once, when component is mounted
+
 	return (
 		<>
 			<Head>
@@ -14,6 +36,7 @@ export default function Home() {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
+			<h1>Socket.io</h1>
 			<main></main>
 		</>
 	);
