d2523af4ed234c0a4ddf3a8ecc38fee9df293597
John Devlin <johnp.devlin2@gmail.com>
2023-12-12 21:13:44 +0000

    remove logic elsewhere for reusability

--- src/components/Layouts/LobbyLayout.tsx
+++ src/components/Layouts/LobbyLayout.tsx
@@ -6,51 +6,41 @@ import InviteDialog from '../Lobby/InviteDialog';
 import InviteButton from '../Lobby/InviteButton';
 import Button from '../Lobby/Button';
 import LobbyButton from '../Lobby/Button';
+import { Dispatch, SetStateAction } from 'react';
+import GamePlayer from '@/classes/player';
+import PlayerGrid from '../PlayerGrid';
+import { PlayerGameStatus, PlayerLobbyStatus } from '@/types/player';
 
-const LobbyLayout = (): JSX.Element => {
+const LobbyLayout = (props: {}): JSX.Element => {
+	const players: PlayerLobbyStatus[] = [
+		{
+			name: 'Jphn',
+			playerID: 'vtniuwn',
+			status: 'ready',
+		},
+		{ name: 'Ger', playerID: '95y2cmn9m', status: 'invited' },
+		{ name: 'Gir', playerID: '052nm', status: 'invited' },
+	];
 	return (
 		<>
 			<Box sx={{ width: '100%' }}>
 				<LinearProgress color='inherit' />
 			</Box>
-			<Box
-				justifyContent='center'
-				alignContent='center'
-				textAlign='center'
-				margin='0 auto'
-				width='80%'>
-				<Grid
-					container
-					flexDirection='row'
-					justifyContent='center'
-					alignItems='center'
-					gap={2}
-					marginBottom='10px'>
-					<Grid xs={5} md={3.3} lg={2.5}>
-						<PlayerElement name={'John'} playerStatus='user' />
-					</Grid>
-					<Grid xs={5} md={3.3} lg={2.5}>
-						<PlayerElement name={'John'} playerStatus='ready' />
-					</Grid>
-					<Grid xs={5} md={3.3} lg={2.5}>
-						<PlayerElement name={'John'} playerStatus='ready' />
-					</Grid>
-					<Grid xs={5} md={3.3} lg={2.5}>
-						<PlayerElement name={'John'} playerStatus='invited' />
-					</Grid>
-					<Grid item xs={10} mt='15px'>
-						<Typography variant='body1'>
-							Waiting for players to join...
-						</Typography>
-					</Grid>
-					<Grid item xs={5}>
-						<InviteButton />
-					</Grid>
-					<Grid item xs={5}>
-						<LobbyButton text={'Start Game'} handleClick={() => {}} />
-					</Grid>
-				</Grid>
-			</Box>
+			<PlayerGrid
+				players={players}
+				message={'Waiting for players to join...'}
+				buttons={
+					<>
+						<Grid item xs={5}>
+							<InviteButton />
+						</Grid>
+						<Grid item xs={5}>
+							<LobbyButton text={'Start Game'} handleClick={() => {}} />
+						</Grid>
+					</>
+				}
+			/>
+
 			<Box sx={{ width: '100%' }}>
 				<LinearProgress color='inherit' />
 			</Box>
